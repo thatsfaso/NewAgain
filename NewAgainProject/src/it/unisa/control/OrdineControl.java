9a0f7a912f58a3ab3442429181a928e59e3180ba
GiuSeppE898 <117904487+GiuSeppE898@users.noreply.github.com>
2023-06-18 13:05:47 +0200

    Update OrdineControl.java

--- NewAgainProject/src/it/unisa/control/OrdineControl.java
+++ NewAgainProject/src/it/unisa/control/OrdineControl.java
@@ -93,7 +93,7 @@ public class OrdineControl extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		doGet(request, response);
 	}
 
