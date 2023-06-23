3e495c04c7153b54e50045aee9b420e736e992f6
thatsfaso <127889824+thatsfaso@users.noreply.github.com>
2023-06-23 18:08:10 +0200

    FINAL COMMIT

--- NewAgainProject/db/sito.sql
+++ NewAgainProject/db/sito.sql
@@ -88,6 +88,51 @@ INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("
 INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("MAGLIETTA SPORTIVA DENVER BRONCOS NFL - L",41, 0, 'M', "T-shirt sportiva NFL dei Denver Broncos di colore blu navy.", "maglie");
 INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("MAGLIA ATOMC STAMPA CANADA A MANICHE LUNGHE - L",55, 0, 'M', "Maglia multicolor Canada stampata Atomc a maniche lunghe completa di scollo a V.", "maglie");
 
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("GIACCA IN PELLE SCAMOSCIATA NERA - L",116, 0, 'M', "La giacca in pelle scamosciata è disponibile in nero con fodera completa e tasche multiple.", "giacche");
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("GIACCA IN NYLON CON ZIP FRONTALE E APPLICAZIONE PATCHWORK ROSSA - L",66, 0, 'M', "Giacca in nylon patchwork rosso con zip frontale completa di design applicato e tasche multiple.", "giacche");
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("GIACCA IN NYLON A CONTRASTO NERO E GIALLO NHL - M",68, 0, 'M', "Giacca NHL nera e gialla in nylon completa di fodera completa e tasche multiple.", "giacche");
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("GIACCA IN PELLE SCAMOSCIATA DEI NY YANKEES NERA E BLU CON ZIP SUL DAVANTI - L",55, 0, 'M', "Giacca in pelle scamosciata nera e blu NY Yankees con cerniera frontale completa di fodera completa e tasche multiple.", "giacche");
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("GIUBBOTTO BOMBER NERO E ROSSO STARTER NBA BULLS CHICAGO DESIGN - S",161, 0, 'M', "Bomber Starter NBA Bulls Chicago design nero e rosso a contrasto completo di fodera trapuntata.", "giacche");
+
+
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("FELPA CON CAPPUCCIO STAMPATA MICHIGAN - L",36, 0, 'M', "Felpa con cappuccio stampata Michigan in blu navy con coulisse al collo e una tasca.", "felpe");
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("FELPA CON CAPPUCCIO STAMPATA PATCHWORK DAYTON UD - L",44, 0, 'M', "La felpa con cappuccio stampata patchwork dayton ud è disponibile in rosso con collo con coulisse e una tasca.", "felpe");
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("FELPA TIE-DYE CON CAPPUCCIO - L",26, 0, 'M', "Felpa con cappuccio multicolore tie-dye con una tasca.", "felpe");
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("FELPA RICAMATA OHIO STATE BUCKEYES - L",46, 0, 'M', "Felpa ricamata Ohio State Buckeyes. Disponibile in nero con cappuccio e una tasca.", "felpe");
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("FELPA STAMPATA FRUIT OF THE LOOM TRINITY UNIVERSITY - XL",46, 0, 'M', "Felpa Fruit Of The Loom Trinity University bordeaux con stampa e maniche raglan.", "felpe");
+
+
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("CAPPELLINO RICAMATO NAVY TEAM MLB - XS",26, 0, 'M', "Cappellino ricamato della squadra MLB. Disponibile in blu navy e rosso.", "accessori");
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("CAPPELLINO RICAMATO NERO - XS",16, 0, 'M', "Berretto ricamato in nero.", "accessori");
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("CAPPELLINO STAMPEDE - XS",22, 0, 'M', "Il cappellino Stampede è multicolore con ricami.", "accessori");
+
+
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("PANTALONCINI DI JEANS NERI CLASSICI LEVI'S - W30 L9",36, 0, 'M', "Shorts in denim Levi's neri con tasche multiple.", "pantaloni");
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("PANTALONI NERI IN DENIM ANNI '90 - W36 L13",34, 0, 'M', "Jorts in denim nero con tasche multiple.", "pantaloni");
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("HUGO BOSS PANTALONI CHINO CLASSICI BEIGE - W32 L32",46, 0, 'M', "Chino classici beige completi di pieghe e tasche multiple. Del marchio Hugo Boss.", "pantaloni");
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("CHINO CLASSICI NERI - W30 L30",47, 0, 'M', "Classici pantaloni chino neri completi di tasche multiple.", "pantaloni");
+
+
+
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("T-SHIRT A FASCIA STAMPATA A MANICHE LUNGHE NERA E ROSA - XL",33, 0, 'F', "T-shirt con fascia a maniche lunghe Lil Uzi nera e rosa. ", "maglie");
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("T-SHIRT STAMPATA GREAT BARRIER REEF NERA - XL",46, 0, 'F', "T-shirt nera con stampa Great Barrier Reef.", "maglie"); 
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("T-SHIRT STAMPATA CON DESIGN DA BASEBALL PUMA BLU NAVY E ROSSO - XL",46, 0, 'F', "T-shirt da baseball blu navy e rossa con stampa Puma completa di maniche raglan.", "maglie");
+
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("THE NORTH FACE PLUM FLEECE LINED NYLON JACKET - L",146, 0, 'F', "Giacca The North Face con zip sul davanti color prugna completa di fodera in pile.", "giacche");
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("GIACCA IN ECOPELLE NERA E ROSSA A CONTRASTO - 16",111, 0, 'F', "La giacca classica è disponibile in nero e rosso completa di fodera in finto shearling e tasche multiple.", "giacche");
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("GIACCA IN PELLE MARRONE - L",100, 0, 'F', "Giacca in pelle monopetto marrone con fodera completa e due tasche.", "giacche");
+
+
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("FELPA CON CAPPUCCIO STAMPATA CHAMPION VIRGINIA LACROSSE - L",38, 0, 'F', "La felpa con cappuccio stampata Champion Virginia Lacrosse è disponibile in blu navy con collo con coulisse e una tasca.", "felpe");
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("FELPA CON CAPPUCCIO NIKE BLU NAVY - L",46, 0, 'F', "La felpa tinta unita è disponibile in blu navy con una tasca.", "felpe");
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("FELPA RICAMATA GEOGRAFIA - L",46, 0, 'F', "Felpa con geografia ricamata. Disponibile in nero con cappuccio e una tasca.", "felpe");
+
+
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("PANTALONI FANTASIA MULTICOLORE RAFAEL - W30 L24",46, 0, 'F', "Pantalone fantasia multicolor a vita alta.", "pantaloni");
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("PANTALONI CON STAMPA FLOREALE - W32 L32",46, 0, 'F', "Pantaloni blu navy con stampa floreale e vita elasticizzata.", "pantaloni");
+INSERT INTO product (nome,prezzo,quantita,sesso,descrizione,categoria) VALUES ("PANTALONI STAMPATI A RIGHE - W26 L29",46, 0, 'F', "Pantaloni stampati a righe multicolor a vita alta.", "pantaloni");
+
+
 
 INSERT INTO immagini (codprodotto) VALUES (1);
 INSERT INTO immagini (codprodotto) VALUES (1);
